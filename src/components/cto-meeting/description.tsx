dfbb6183c1715ae8c0c4b8e624dff27c836e30b2
Feldrise <admin@feldrise.com>
2023-11-01 16:36:00 +0100

    🎨 Prettify the code

--- src/components/cto-meeting/description.tsx
+++ src/components/cto-meeting/description.tsx
@@ -1,16 +1,13 @@
 import {component$} from '@builder.io/qwik';
 
-
 export default component$(() => {
 	return (
-		<section class="max-w-desktop px-8 pt-8 pb-8 mb-4">
-			<p class="text-xl">
-				Dans le cadre du salon, nous vous proposons de nous laisser vos coordonnées pour que nous vous recontactions plus tard.
-			</p>
+		<section class="mb-4 max-w-desktop px-8 pb-8 pt-8">
+			<p class="text-xl">Dans le cadre du salon, nous vous proposons de nous laisser vos coordonnées pour que nous vous recontactions plus tard.</p>
 			<div class="h-8" />
-			<p class="bg-primary text-white rounded-md p-8">
-				<strong>Vous avez un projet dont vous souhaitez discuter avec un de nos CTO ?</strong> À la fin de ce formulaire, vous pourrez
-				prendre rendez-vous <strong>sur un créneau pendant le salon.</strong>
+			<p class="rounded-md bg-primary p-8 text-white">
+				<strong>Vous avez un projet dont vous souhaitez discuter avec un de nos CTO ?</strong> À la fin de ce formulaire, vous pourrez prendre rendez-vous{' '}
+				<strong>sur un créneau pendant le salon.</strong>
 			</p>
 		</section>
 	);
